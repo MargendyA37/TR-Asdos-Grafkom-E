e3b30581e41812f5ccc5c9d7513d08c614f6fd00
MargendyA37 <68418605+MargendyA37@users.noreply.github.com>
2020-07-20 21:00:38 +0700

    source_code
    fungsi keyboard, mouse, void display, void init, void, ukuran,  void main

---
+++ main.cpp
@@ -0,0 +1,141 @@
+#include <windows.h>
+#include <stdlib.h>
+#include <gl/glut.h>
+
+void Keyboard(unsigned char, int, int);
+
+float xrot = 0.0f;
+float yrot = 0.0f;
+float xdiff = 0.0f;
+float ydiff = 0.0f;
+
+bool mouseDown = false;
+int is_depth;
+void init() {
+ glClearColor(0.5, 0.5, 1.0, 1.0);
+	glClearDepth(5.0f);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_COLOR_MATERIAL);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glShadeModel(GL_SMOOTH);
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+}
+
+void tampil() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glRotatef(xrot, 1.0f, 0.0f, 0.0f);
+	glRotatef(yrot, 0.0f, 1.0f, 0.0f);
+	glPushMatrix();
+	
+
+
+
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+void mouse(int button, int state, int x, int y){
+	if (button == GLUT_LEFT_BUTTON){
+			mouseDown = true;
+			xdiff = x - yrot;
+			ydiff = -y + xrot;
+		}
+	else
+	mouseDown = false;
+}
+ 
+void mouseMotion(int x, int y){
+	if (mouseDown){
+		yrot = x - xdiff;
+		xrot = y + ydiff;
+		glutPostRedisplay();
+	}
+}
+
+void ukuran(GLsizei width, GLsizei height) {
+	if (height == 0) height = 1;
+	GLfloat aspect = (GLfloat)width / (GLfloat)height;
+	glViewport(0, 0, width, height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f, aspect, 0.1f, 100.0f);
+}
+
+void Keyboard(unsigned char key, int x, int y)
+{
+    switch (key)
+    {
+    case 'w':
+    case 'W':
+        glTranslatef(0.0, 0.0, 3.0);
+        break;
+    case 'd':
+    case 'D':
+        glTranslatef(3.0, 0.0, 0.0);
+        break;
+    case 's':
+    case 'S':
+        glTranslatef(0.0, 0.0, -3.0);
+        break;
+    case 'a':
+    case 'A':
+        glTranslatef(-3.0, 0.0, 0.0);
+        break;
+    case '7':
+        glTranslatef(0.0, 3.0, 0.0);
+        break;
+    case '9':
+        glTranslatef(0.0, -3.0, 0.0);
+        break;
+    case '2':
+        glRotatef(2.0, 1.0, 0.0, 0.0);
+        break;
+    case '8':
+        glRotatef(-2.0, 1.0, 0.0, 0.0);
+        break;
+    case '6':
+        glRotatef(2.0, 0.0, 1.0, 0.0);
+        break;
+    case '4':
+        glRotatef(-2.0, 0.0, 1.0, 0.0);
+        break;
+    case '1':
+        glRotatef(2.0, 0.0, 0.0, 1.0);
+        break;
+    case '3':
+        glRotatef(-2.0, 0.0, 0.0, 1.0);
+        break;
+    case '5':
+        if (is_depth)
+        {
+            is_depth = 0;
+            glDisable(GL_DEPTH_TEST);
+        }
+        else
+        {
+            is_depth = 1;
+            glEnable(GL_DEPTH_TEST);
+        }
+    }
+    tampil();
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE);
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(50, 50);
+	glutCreateWindow("Margendy Albertinus Amisi - 672018339");
+	glutDisplayFunc(tampil);
+	glutReshapeFunc(ukuran);
+	glutKeyboardFunc(Keyboard);
+	glutMouseFunc(mouse);
+	glutMotionFunc(mouseMotion);
+	init();
+	glutMainLoop();
+	return 0;
+}
